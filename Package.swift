bcd05f7c6d4bfb670ed9df38096c280b6d2af9fa
zhangyanbin <zhangyanbin1@xiaomi.com>
2020-07-30 20:23:50 +0800

    init

--- Package.swift
+++ Package.swift
@@ -4,25 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+    name: "MyFramework",
+    platforms: [
+        .iOS(.v11)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(
+            name: "MyFramework",
+            targets: ["MyFramework"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary",
-            dependencies: []),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
-    ]
-)
+        .binaryTarget(
+            name: "MyFramework",
+            path: "MyFramework.xcframework")
+    ])
+
